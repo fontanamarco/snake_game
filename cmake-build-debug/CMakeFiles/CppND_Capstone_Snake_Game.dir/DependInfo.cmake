09a930663ea8a3784f967924f22aa150b9f44f2b
Marco Fontana <fontanamarco@email.it>
2019-08-06 15:42:08 +0200

    Fist commit of the project
    Fixed a bug in game.cpp constructor:
    the x and y random values went from 0 to 32 insted of 0 to 31

---
+++ cmake-build-debug/CMakeFiles/CppND_Capstone_Snake_Game.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/xilinx/github/CppND-Capstone-Snake-Game/src/controller.cpp" "/home/xilinx/github/CppND-Capstone-Snake-Game/cmake-build-debug/CMakeFiles/CppND_Capstone_Snake_Game.dir/src/controller.cpp.o"
+  "/home/xilinx/github/CppND-Capstone-Snake-Game/src/game.cpp" "/home/xilinx/github/CppND-Capstone-Snake-Game/cmake-build-debug/CMakeFiles/CppND_Capstone_Snake_Game.dir/src/game.cpp.o"
+  "/home/xilinx/github/CppND-Capstone-Snake-Game/src/main.cpp" "/home/xilinx/github/CppND-Capstone-Snake-Game/cmake-build-debug/CMakeFiles/CppND_Capstone_Snake_Game.dir/src/main.cpp.o"
+  "/home/xilinx/github/CppND-Capstone-Snake-Game/src/renderer.cpp" "/home/xilinx/github/CppND-Capstone-Snake-Game/cmake-build-debug/CMakeFiles/CppND_Capstone_Snake_Game.dir/src/renderer.cpp.o"
+  "/home/xilinx/github/CppND-Capstone-Snake-Game/src/snake.cpp" "/home/xilinx/github/CppND-Capstone-Snake-Game/cmake-build-debug/CMakeFiles/CppND_Capstone_Snake_Game.dir/src/snake.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/CppND_Capstone_Snake_Game.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CppND_Capstone_Snake_Game.dir/src/controller.cpp.o"
+  "CMakeFiles/CppND_Capstone_Snake_Game.dir/src/game.cpp.o"
+  "CMakeFiles/CppND_Capstone_Snake_Game.dir/src/main.cpp.o"
+  "CMakeFiles/CppND_Capstone_Snake_Game.dir/src/renderer.cpp.o"
+  "CMakeFiles/CppND_Capstone_Snake_Game.dir/src/snake.cpp.o"
+  "CppND_Capstone_Snake_Game.pdb"
+  "CppND_Capstone_Snake_Game"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CppND_Capstone_Snake_Game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
